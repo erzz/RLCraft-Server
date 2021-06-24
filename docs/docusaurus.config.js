ac67cb5747d59afb2be92850ba0216fccaf697d1
erzz <sean@erzz.com>
2021-06-24 20:58:36 +0200

    docs: fix some links

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -72,7 +72,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/ingka-group-digital/retail-pim-admin',
             },
           ],
         },
@@ -92,13 +92,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/erzz/RLCraft-Server/edit/master/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/erzz/RLCraft-Server/edit/master/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
